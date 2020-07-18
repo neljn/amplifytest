16ff6209203b138777dd0bf773630bdfed2eebb7
neljn <neljn@amazon.com>
2020-07-18 21:43:04 +1000

    Fixed push issues.
    Re pushed simple setup for components etc

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,6 @@ import Aux from '../../hoc/Aux';
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-
         <main>
             {props.children}
         </main>
